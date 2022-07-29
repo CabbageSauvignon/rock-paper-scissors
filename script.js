4da1d5711b92530ba59f39767a92da5cb0f6a44b
CabbageSauvignon <nowak.jeremy.d@gmail.com>
2022-07-28 22:15:50 -0700

    Complete game logic

--- script.js
+++ script.js
@@ -15,31 +15,50 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice == "rock" && computerChoice == "rock") {
-        
+        return "Tie game.";
     } else if (playerChoice == "rock" && computerChoice == "paper") {
-        
+        return "Computer Wins...Paper beats Rock";
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
-
+        return "You win! Rock beats Scissors.";
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-
+        return "You win! Paper beats Rock.";
     } else if (playerChoice == "paper" && computerChoice == "paper") {
-
+        return "Tie game.";
     } else if (playerChoice == "paper" && computerChoice == "scissors") {
-
+        return "Computer wins...Scissors beat Paper.";
     } else if (playerChoice == "scissors" && computerChoice == "rock") {
-
+        return "Computer wins...Rock beats Scissors.";
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
-
+        return "You win! Scissors beats Paper.";
     } else if (playerChoice == "scissors" && computerChoice == "scissors") {
-
+        return "Tie game.";
     }
-    
-    
-    
-    console.log(playerChoice);
-    console.log(computerChoice);
 }
 
-let playerChoice = prompt("Choose Rock, Paper, or Scissors");
-let computerChoice = getComputerChoice();
-playRound(playerChoice,computerChoice);
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerChoiceRaw = prompt("Choose Rock, Paper, or Scissors");
+        let playerChoice = playerChoiceRaw.toLowerCase();
+        let computerChoice = getComputerChoice();
+        let result = playRound(playerChoice,computerChoice);
+        console.log(i);
+        console.log(result);
+        if (result.charAt(0) == "Y") {
+            playerScore++;
+        } 
+        else if (result.charAt(0) == "C") {
+            computerScore++;
+        }
+    }
+    if (playerScore > computerScore) {
+        return "You Won!"
+    }
+    else if (computerScore > playerScore) {
+        return "You lose! Better luck next time."
+    }
+}
+
+let result = game();
+console.log(result);
